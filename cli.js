d4e09b6096749f1ea1b7d92d5a3eecdac58bd781
gzs_iceberg <gzs_iceberg@outlook.com>
2026-02-11 12:20:33 +0800

    Implement blog model and API endpoints for CRUD operations

--- cli.js
+++ cli.js
@@ -1,11 +1,47 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, DataTypes, Model } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    author: {
+      type: DataTypes.TEXT
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    }
+  },
+  {
+    sequelize,
+    modelName: 'blog',
+    tableName: 'blogs',
+    timestamps: false
+  }
+)
+
 const main = async () => {
   try {
-    const [rows] = await sequelize.query('SELECT * FROM blogs')
+    const rows = await Blog.findAll({
+      order: [['id', 'ASC']]
+    })
 
     rows.forEach((blog) => {
       console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
